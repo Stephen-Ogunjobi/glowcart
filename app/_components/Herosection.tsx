9f7b732bf9b070e694db9c88523f2b059f197962
stephen-ogunjobi <stephenogunjobi400@gmail.com>
2025-08-05 23:33:35 +0800

    hero section

--- app/_components/Herosection.tsx
+++ app/_components/Herosection.tsx
@@ -5,8 +5,9 @@ export default function Herosection() {
   return (
     <section className="flex h-[90vh] bg-gradient-to-r from-[var(--color-beige)] to-[var(--color-offwhite)]">
       <div className="w-[60%] bg-[var(--color-pink)] bg-opacity-10 p-16 flex flex-col justify-center items-start space-y-8">
-        <h2 className="text-6xl font-playfair text-[var(--color-rose-gold)] leading-tight">
-          Glow Starts <span className="text-7xl block mt-2">Here</span>
+        <h2 className="text-6xl font-playfair text-[#4A071C] leading-tight font-bold text-shadow-sm">
+          Glow Starts{" "}
+          <span className="text-7xl block mt-2 font-bold">Here</span>
         </h2>
         <p className="text-xl text-[var(--color-rose-gold)] opacity-80 max-w-lg">
           Your journey to radiant skin begins here. Explore our range of
